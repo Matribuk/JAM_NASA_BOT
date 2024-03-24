668e6c2510cfa042cf8b2325d343753a4001f660
Sonny-Fourmont <sonny.fourmont@epitech.eu>
2024-03-25 00:41:26 +0100

    [FIX] error handling if APOD doesn't exist

--- ts/commands/apod.ts
+++ ts/commands/apod.ts
@@ -7,14 +7,20 @@ async function apod(interaction: any) {
             exec(`python3 ./assets/script/apod-maker.py ${formattedDate}`, async (error, stdout, stderr) => {
                 await interaction.deferReply();
                 console.log(stdout);
-                await interaction.editReply({files: [`./assets/images/APOD/iod-${formattedDate}.jpg`]});
+                if (!(stdout == "Image processing complete."))
+                    await interaction.editReply("Astronomy Picture Of The Day is not available yet");
+                else
+                    await interaction.editReply({files: [`./assets/images/APOD/iod-${formattedDate}.jpg`]});
             });
         } else {
             const arg = interaction.options.get('date').value;
             await interaction.deferReply();
             exec(`python3 ./assets/script/apod-maker.py ${arg}`, async (error, stdout, stderr) => {
                 console.log(stdout);
-                await interaction.editReply({files: [`./assets/images/APOD/iod-${arg}.jpg`]});
+                if (!(stdout == "Image processing complete."))
+                    await interaction.editReply(`Astronomy Picture Of The Day for the "${arg}" doesn't exist or is not available`);
+                else
+                    await interaction.editReply({files: [`./assets/images/APOD/iod-${arg}.jpg`]});
             });
         }
     }
