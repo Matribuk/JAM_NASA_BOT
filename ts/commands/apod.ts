79fa184b12ac9749777154ee47a3540bcc4c3f76
Sonny-Fourmont <sonny.fourmont@epitech.eu>
2024-03-24 21:37:37 +0100

    [MV] move all the discord bot outside the bot dir

--- ts/commands/apod.ts
+++ ts/commands/apod.ts
@@ -5,14 +5,16 @@ async function apod(interaction: any) {
     if (interaction.commandName === "apod") {
         if (!interaction.options.get('date')) {
             exec(`python3 ./assets/script/apod-maker.py ${formattedDate}`, async (error, stdout, stderr) => {
+                await interaction.deferReply();
                 console.log(stdout);
-                await interaction.reply({files: [`./assets/images/APOD/iod-${formattedDate}.jpg`]});
+                await interaction.editReply({files: [`./assets/images/APOD/iod-${formattedDate}.jpg`]});
             });
         } else {
             const arg = interaction.options.get('date').value;
+            await interaction.deferReply();
             exec(`python3 ./assets/script/apod-maker.py ${arg}`, async (error, stdout, stderr) => {
                 console.log(stdout);
-                await interaction.reply({files: [`./assets/images/APOD/iod-${arg}.jpg`], contentType: "image"});
+                await interaction.editReply({files: [`./assets/images/APOD/iod-${arg}.jpg`]});
             });
         }
     }
