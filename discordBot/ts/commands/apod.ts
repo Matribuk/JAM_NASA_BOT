9cb7c5d58d1d8bd0c9589e389235996b2ed71001
Sonny-Fourmont <sonny.fourmont@epitech.eu>
2024-03-24 18:05:10 +0100

    [FIX] APOD command

--- discordBot/ts/commands/apod.ts
+++ discordBot/ts/commands/apod.ts
@@ -4,13 +4,15 @@ import { exec } from "child_process";
 async function apod(interaction: any) {
     if (interaction.commandName === "apod") {
         if (!interaction.options.get('date')) {
-            exec(`python3 ./assets/script/apod-maker.py ${formattedDate}`, () => {
-                interaction.reply({files: [`./assets/images/APOD/iod-${formattedDate}.jpg`]});
+            exec(`python3 ./assets/script/apod-maker.py ${formattedDate}`, async (error, stdout, stderr) => {
+                console.log(stdout);
+                await interaction.reply({files: [`./assets/images/APOD/iod-${formattedDate}.jpg`]});
             });
         } else {
             const arg = interaction.options.get('date').value;
-            exec(`python3 ./assets/script/apod-maker.py ${arg}`, () => {
-                interaction.reply({files: [`./assets/images/APOD/iod-${arg}.jpg`], contentType: "image"});
+            exec(`python3 ./assets/script/apod-maker.py ${arg}`, async (error, stdout, stderr) => {
+                console.log(stdout);
+                await interaction.reply({files: [`./assets/images/APOD/iod-${arg}.jpg`], contentType: "image"});
             });
         }
     }
